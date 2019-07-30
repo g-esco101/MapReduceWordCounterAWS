33ebc83af4770977de68e05b5c47189ff848da5a
g-esco101 <govinda.escobar@gmail.com>
2019-07-30 05:08:58 -0700

    Add files via upload
    Combine Service Project

---
+++ CombineService/IService1.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+
+
+namespace CombineService
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        // Implementation of the Combine service interface.
+        [OperationContract]
+        int CombineFunction(IDictionary<string, int> reduceOutput); //endpoint
+    }
+}
---
+++ CombineService/Service1.svc.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+
+
+namespace CombineService
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
+    public class Service1 : IService1
+    {
+        // Adds the values of a dictionary of key-value pair where the key is the thread
+        // id & the value is the total number of word occurrences & returns the sum.
+        public int CombineFunction(IDictionary<string, int> reduceOutput)
+        {
+            int sum = 0;
+            try
+            {
+                foreach (var element in reduceOutput)
+                {
+                    sum += element.Value;
+                }
+            }
+            catch { }
+            return sum;
+        }
+    }
+}
