4d340f4a1655ed2b1b354006f5b717d24acbf04d
g-esco101 <govinda.escobar@gmail.com>
2019-07-30 05:10:56 -0700

    Add files via upload
    Map Service Project

---
+++ MapService/IService1.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+
+
+namespace MapService
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        // Implementation of the Map service interface.
+        [OperationContract]
+        IDictionary<string, int> MapFunction(string[] wordsArray);  // endpoint
+    }
+}
---
+++ MapService/Service1.svc.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace MapService
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
+    public class Service1 : IService1
+    {
+        // Maps an array of words to a dictionary of key-value pairs where
+        // each word is a key & the value is the number of times the word occurs in wordsArray. 
+        public IDictionary<string, int> MapFunction(string[] wordsArray)
+        {
+            IDictionary<string, int> mapReturn = new Dictionary<string, int>();
+            try
+            {
+                foreach (string word in wordsArray)
+                {
+                    if (mapReturn.ContainsKey(word))
+                    {
+                        mapReturn[word] = mapReturn[word] + 1;
+                    }
+                    else mapReturn.Add(word, 1);
+                }
+            }
+            catch
+            {
+                mapReturn.Add("MAP SERVICE ERROR", 0);
+            }
+            return mapReturn;
+        }
+    }
+}
