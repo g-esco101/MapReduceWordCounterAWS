9f2b39c57852e03cee87f7102238b36ddab040b6
g-esco101 <govinda.escobar@gmail.com>
2019-07-30 05:15:29 -0700

    Add files via upload
    Reduce Service Project

---
+++ ReduceService/IService1.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace ReduceService
+{
+
+    [ServiceContract]
+    public interface IService1
+    {
+
+        // Implementation of the Reduce service interface.
+        [OperationContract]
+        IDictionary<string, int> ReduceFunction(IDictionary<string, int> wordDictionary);   // endpoint
+    }
+
+}
---
+++ ReduceService/Service1.svc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.Threading;
+
+
+namespace ReduceService
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
+    public class Service1 : IService1
+    {
+        // Reduces a dictionary of key-value pairs where each word is a key & the value is the number of times the word occurred
+        // to a single key-value pair where the key is the thread id & the value is the total number of word occurrences.
+        public IDictionary<string, int> ReduceFunction(IDictionary<string, int> wordDictionary)
+        {
+            IDictionary<string, int> reduceReturn = new Dictionary<string, int>();
+            int sum = 0;
+            string key;
+            try
+            {
+                foreach (var element in wordDictionary)
+                {
+                    sum += element.Value;
+                }
+                key = Convert.ToString(Thread.CurrentThread.ManagedThreadId);
+            }
+            catch
+            {
+                key = "REDUCE SERVICE ERROR";
+                sum = 0;
+            }
+            reduceReturn.Add(key, sum);
+            return reduceReturn;
+        }
+    }
+}
